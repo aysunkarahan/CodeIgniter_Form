96d0c6cf23a6c709a91e3f828f1b0349f28dbf27
aysunkarahan <aysunnkarahan@gmail.com>
2017-04-28 16:31:11 +0300

    user friendly form with angular.js

--- application/views/form.php
+++ application/views/form.php
@@ -16,33 +16,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <h3>Başvuru Formu</h3>
     <h4>Contact us for custom quote</h4>
     <fieldset>
-      <input ng-model="name" placeholder="Your name" name="username" type="text" tabindex="1" value="<?php echo set_value('username'); ?>" required autofocus>
-      <p>You entered: {{name}}</p>
+      <input ng-model="name" placeholder="Your name" name="username" type="text" tabindex="1" value="<?php echo set_value('username'); ?>" required autofocus>      
     </fieldset>
     <fieldset>
-      <input ng-model="email" placeholder="Your Email Address" name="email" type="email" value="<?php echo set_value('email'); ?>" tabindex="2" required>
-      <p>You entered: {{email}}</p>
+      <input ng-model="email" ng-show="name" placeholder="Your Email Address" name="email" type="email" value="<?php echo set_value('email'); ?>" tabindex="2" required>
+          </fieldset>
+    <fieldset>
+      <input ng-model="tel" ng-show="email" placeholder="Your Phone Number (optional)" name="phone" value="<?php echo set_value('phone'); ?>" type="tel" tabindex="3" required>      
     </fieldset>
     <fieldset>
-      <input ng-model="tel"  placeholder="Your Phone Number (optional)" name="phone" value="<?php echo set_value('phone'); ?>" type="tel" tabindex="3" required>
-      <p>You entered: {{tel}}</p>
+      <input ng-model="url" ng-show="tel" placeholder="Your Web Site (optional)" name="url" value="<?php echo set_value('url'); ?>"type="url" tabindex="4" required>     
     </fieldset>
     <fieldset>
-      <input ng-model="url" placeholder="Your Web Site (optional)" name="url" value="<?php echo set_value('url'); ?>"type="url" tabindex="4" required>
-      <p>You entered: {{url}}</p>
-    </fieldset>
-
-    <fieldset>
-      <textarea ng-model="mesaj" placeholder="Type your message here...." name="mesaj" value="<?php echo set_value('mesaj'); ?>"tabindex="5" required></textarea>
-      <p>You entered: {{mesaj}}</p>
+      <textarea ng-model="mesaj" ng-show="url" placeholder="Type your message here...." name="mesaj" value="<?php echo set_value('mesaj'); ?>"tabindex="5" required></textarea>     
     </fieldset>
     <fieldset>
-      <div><strong>Resim Seçin:</strong> <input type="file" name="resim" /></div>
+      <div><strong ng-show="mesaj">Resim Seçin:</strong> <input ng-model="file" ng-show="mesaj" type="file" name="resim" /></div>
     </fieldset>
     <fieldset>
-      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-    </fieldset>
-   
+      <button ng-show="mesaj" name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>   
   </form>
 </div>
 </body>
